5e70780fa62415f3b1847fb8082610adbf3245ab
BlueTinHourglass <49942596+BlueTinHourglass@users.noreply.github.com>
2025-11-30 18:18:17 +0800

    Visual changes.

--- CarRegistryApp.Server/Car.cs
+++ CarRegistryApp.Server/Car.cs
@@ -6,6 +6,10 @@
 
         public string Make { get; set; }
 
+        public string Colour { get; set; }
+
+        public string Model { get; set; }
+
         //public bool ExpiryStatus => ExpiryDate >= DateOnly.FromDateTime(DateTime.Now);
 
         public bool Expired { get; set; }
--- carregistryapp.client/src/app/models/car.model.ts
+++ carregistryapp.client/src/app/models/car.model.ts
@@ -1,6 +1,8 @@
 export interface Car {
   expiryDate: string;
   make: string;
+  colour: string;
+  model: string;
   registration: string;
   expired: boolean;
   expiryStatus?: string;
